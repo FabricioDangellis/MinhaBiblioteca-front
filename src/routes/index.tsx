acee7eadbf0ed793171a2c2d1e6af42a1636ddb8
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2026-03-31 08:55:23 -0300

    add: Adicionando tela de login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Cadastro from "../pages/Cadastro/Index";
+import Cadastro from "../pages/Cadastro/index";
+import Login from "../pages/Login";
 
 export default function AppRoutes() {
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Cadastro/>}/>
+                <Route path="/" element={<Login/>}/>
+                <Route path="/cadastro" element={<Cadastro/>}/>
             </Routes>
         </BrowserRouter>
     );
